34165c33af06f7bdb9d6f60b1e626ca5603d2531
TenZe Inc <33192847+TenZeInc@users.noreply.github.com>
2018-11-06 16:18:14 -0500

    Add files via upload

---
+++ tork.js
@@ -0,0 +1,10 @@
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+
+function login(){
+    if(username === "Fluffy"){
+        if(password === "fluffythecoolkid"){
+            document.getElementById('')
+        }
+    }
+}
